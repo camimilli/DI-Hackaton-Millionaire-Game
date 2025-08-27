58e8d694e9c6c4aa4afd74ba49382cc4bd9575ac
camimilli <cami.milli@gmail.com>
2025-08-27 13:42:20 +0300

    fixing conflicts

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 
 # List of questions and money ladder 
-questions_money_ladder = [(1, 100),(2, 200),(3, 300),(4, 500),(5, 1000),
-                          (6, 2000),(7, 4000),(8, 8000), (9, 16000), (10, 32000),
-                          (11, 64000), (12, 125000), (13, 250000), (14, 500000), (15, 1000000)]
+questions_money_ladder = [(100, 'easy'),(200, 'easy'),(300, 'easy'),(500, 'easy'),(1000, 'easy'),
+                          (2000, 'medium'),(4000, 'medium'),(8000, 'medium'), (16000, 'medium'), (32000, 'medium'),
+                          (64000, 'hard'), (125000, 'hard'), (250000, 'hard'), (500000, 'hard'), (1000000, 'hard')]
 
 
 # welcome message 
