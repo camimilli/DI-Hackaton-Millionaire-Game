a971e4e73c5eca60d7d15526f159d9a106cf9275
camimilli <cami.milli@gmail.com>
2025-08-27 10:32:33 +0300

    gitignore file, changes in main.py with api request

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-print('Hello')
+import requests
+import json
+import os
+import psycopg2
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+api = 'https://opentdb.com/api.php?amount=10'
+
+
+def get_questions_info():
+    '''
+    Requests questions from API Trivia 
+    and stores them in a json file 
+    '''
+    response = requests.get(api)
+
+    if response.status_code == 200:
+        questions_data = json.loads(response.text)
+        with open(f'{dir_path}/questions.json', 'w', encoding='utf-8') as f:
+            json.dump(questions_data, f, indent=4, sort_keys=True)
+    else:
+        print('Failed to retrieve data')
+
+
+# add try-except handling 
+
+get_questions_info()
+
+# after each game: 
+#   - drop table if exists 
+
+
+
+
